f7d8725e0af327df31f866b4220c389d56f283e3
alexgaarciia <al.garcia636@gmail.com>
2024-04-12 11:32:05 -0400

    finished checking last exercise

--- Q4-ver2/server.js
+++ Q4-ver2/server.js
@@ -244,7 +244,5 @@ app.post("/find-pets", (req, res) => {
 
 
 // Start listening for requests on the specified port
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = 5249;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
